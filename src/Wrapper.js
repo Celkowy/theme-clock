92c733ff70840a9ec884436d45d2c69915a83554
tucelek@gmail.com <tucelek@gmail.com>
2022-06-12 16:31:22 +0200

    css ench

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -4,6 +4,11 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
+  padding: 50px;
+  border-radius: 5px;
+  -webkit-box-shadow: 0px 0px 24px 0px rgba(66, 68, 90, 1);
+  -moz-box-shadow: 0px 0px 24px 0px rgba(66, 68, 90, 1);
+  box-shadow: 0px 0px 24px 0px rgba(66, 68, 90, 1);
 `
 
 export default Wrapper
