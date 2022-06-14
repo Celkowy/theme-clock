ae58c5b5abcbab8c331cd471e33ebf2678edd224
tucelek@gmail.com <tucelek@gmail.com>
2022-06-14 20:43:23 +0200

    box shadow

--- src/Components/Clock/Dot.js
+++ src/Components/Clock/Dot.js
@@ -12,7 +12,7 @@ const Dot = styled.div`
   border-radius: 50%;
   border: 3px solid ${props => (props.theme.mode ? 'red' : 'black')};
   transform: translate(-50%, -50%);
-  box-shadow: ${props => (props.theme.mode ? 'unset' : '0px 0px 15px 5px rgba(66, 68, 90, 1)')};
+  box-shadow: ${props => (props.theme.mode ? 'unset' : '0px 0px 10px 5px rgba(66, 68, 90, 1)')};
   transition: all 0.7s cubic-bezier(0.99, 0, 0.52, 1);
 `
 
